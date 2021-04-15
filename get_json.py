6c12ab65c58fa19e8d9c0450fa9400fba46203e8
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-15 16:32:07 -0300

    versão 01 get_json

--- get_json.py
+++ get_json.py
@@ -6,12 +6,31 @@ from unittest import TestCase, main
 class Blog:
     def __init__(self, nome):
         self.nome = nome 
-
-
+        self.versao = 1 
+ 
     def posts(self):
-        endereco = "www.google.com"
+        endereco = "https://jsonplaceholder.typicode.com/posts"
         response = requests.get(endereco)
         return response.json()
 
     def __repr__(self):
-        pass
+        return f'Blog: {self.nome}'
+
+class Testes(TestCase):
+
+    def teste_download(self):
+        start = time.time()
+        blog = Blog("Teste")
+        result = blog.posts()
+        with open('pessoas.json', 'w') as json_file:
+            json.dump(result, json_file)
+        
+        end = time.time()
+        execution_time = end - start 
+        print(f"{blog.nome} {blog.versao} tempo de execução: {execution_time * 1000:0.2f} mf")
+
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result[0], dict)
+
+if __name__ == '__main__':
+    main()
